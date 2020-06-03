fa8577e72ee7ef69ce8eb4abae78845ba2be57d5
leighad <cdversiontwo@gmail.com>
2020-06-03 15:22:12 -0700

    add route and notes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :users
+  #need to limit to only what is being used in final product 
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

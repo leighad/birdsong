4ace6ae2b30ce3923dfb780dc5aa20b3b8de1b57
leighad <cdversiontwo@gmail.com>
2020-06-03 15:38:57 -0700

    add index and show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,11 @@
 class UsersController < ApplicationController
-    
+    def index 
+        users = User.all
+        render json: users, include: [:birds]
+    end
+
+    def show
+        user = User.find_by_id(params[:id])
+        render json: user, include: [:birds]
+    end
 end

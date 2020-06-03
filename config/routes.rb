df4bdb47bd5a0a85371cc5c6a1be69a615e45281
leighad <cdversiontwo@gmail.com>
2020-06-03 15:43:39 -0700

    add birds route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :users
+  resources :birds 
   #need to limit to only what is being used in final product 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
